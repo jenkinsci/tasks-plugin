c126db7551c6b0ae5c402868832e37935e7a88de
drulli <drulli@dev.java.net>
2007-10-01 20:16:57 +0000

    Refactoring: extracted base class AbstractResultAction to be used with findbugs and tasks plugin.

--- src/main/java/hudson/plugins/tasks/TasksResultAction.java
+++ src/main/java/hudson/plugins/tasks/TasksResultAction.java
@@ -4,16 +4,15 @@ import hudson.model.AbstractBuild;
 import hudson.model.Build;
 import hudson.model.HealthReport;
 import hudson.model.HealthReportingAction;
+import hudson.plugins.tasks.util.AbstractResultAction;
 import hudson.plugins.tasks.util.ChartBuilder;
 import hudson.plugins.tasks.util.HealthReportBuilder;
 import hudson.plugins.tasks.util.PrioritiesAreaRenderer;
 import hudson.plugins.tasks.util.ResultAction;
 import hudson.plugins.tasks.util.ResultAreaRenderer;
-import hudson.util.ChartUtil;
 import hudson.util.DataSetBuilder;
 import hudson.util.ChartUtil.NumberOnlyBuildLabel;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -21,10 +20,6 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.renderer.category.StackedAreaRenderer;
 import org.jfree.data.category.CategoryDataset;
 import org.kohsuke.stapler.StaplerProxy;
-import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.StaplerResponse;
-
-import edu.umd.cs.findbugs.annotations.SuppressWarnings;
 
 /**
  * Controls the live cycle of the task scanner results. This action persists the
@@ -37,18 +32,11 @@ import edu.umd.cs.findbugs.annotations.SuppressWarnings;
  *
  * @author Ulli Hafner
  */
-public class TasksResultAction implements StaplerProxy, HealthReportingAction, ResultAction<TasksResult> {
+public class TasksResultAction extends AbstractResultAction implements StaplerProxy, HealthReportingAction, ResultAction<TasksResult> {
     /** Unique identifier of this class. */
     private static final long serialVersionUID = -3936658973355672416L;
     /** URL to results. */
     private static final String TASKS_RESULT_URL = "tasksResult";
-    /** Height of the graph. */
-    private static final int HEIGHT = 200;
-    /** Width of the graph. */
-    private static final int WIDTH = 500;
-    /** The associated build of this action. */
-    @SuppressWarnings("Se")
-    private final Build<?, ?> owner;
     /** The actual result of the FindBugs analysis. */
     private TasksResult result;
     /** Builds a health report. */
@@ -65,18 +53,9 @@ public class TasksResultAction implements StaplerProxy, HealthReportingAction, R
      *            health builder to use
      */
     public TasksResultAction(final Build<?, ?> owner, final TasksResult result, final HealthReportBuilder healthReportBuilder) {
-        this.owner = owner;
+        super(owner);
         this.result = result;
         this.healthReportBuilder = healthReportBuilder;
-    }
-
-    /**
-     * Returns the associated build of this action.
-     *
-     * @return the associated build of this action
-     */
-    public Build<?, ?> getOwner() {
-        return owner;
     }
 
     /** {@inheritDoc} */
@@ -147,7 +126,7 @@ public class TasksResultAction implements StaplerProxy, HealthReportingAction, R
      * @return the test result of a previous build, or <code>null</code>
      */
     private TasksResultAction getPreviousBuild() {
-        AbstractBuild<?, ?> build = owner;
+        AbstractBuild<?, ?> build = getOwner();
         while (true) {
             build = build.getPreviousBuild();
             if (build == null) {
@@ -181,45 +160,12 @@ public class TasksResultAction implements StaplerProxy, HealthReportingAction, R
     }
 
     /**
-     * Generates a PNG image for the result trend.
-     *
-     * @param request
-     *            Stapler request
-     * @param response
-     *            Stapler response
-     * @throws IOException
-     *             in case of an error in
-     *             {@link TasksResultAction#doGraph(StaplerRequest, StaplerResponse)}
-     */
-    public void doGraph(final StaplerRequest request, final StaplerResponse response) throws IOException {
-        if (ChartUtil.awtProblem) {
-            response.sendRedirect2(request.getContextPath() + "/images/headless.png");
-            return;
-        }
-        ChartUtil.generateGraph(request, response, createChart(), WIDTH, HEIGHT);
-    }
-
-    /**
-     * Generates a PNG image for the result trend.
-     *
-     * @param request
-     *            Stapler request
-     * @param response
-     *            Stapler response
-     * @throws IOException
-     *             in case of an error in
-     *             {@link TasksResultAction#doGraph(StaplerRequest, StaplerResponse)}
-     */
-    public void doGraphMap(final StaplerRequest request, final StaplerResponse response) throws IOException {
-        ChartUtil.generateClickableMap(request, response, createChart(), WIDTH, HEIGHT);
-    }
-
-    /**
      * Creates the chart for this action.
      *
      * @return the chart for this action.
      */
-    private JFreeChart createChart() {
+    @Override
+    protected JFreeChart createChart() {
         ChartBuilder chartBuilder = new ChartBuilder();
         StackedAreaRenderer renderer;
         if (healthReportBuilder == null) {
@@ -249,7 +195,7 @@ public class TasksResultAction implements StaplerProxy, HealthReportingAction, R
                 List<Integer> series = healthReportBuilder.createSeries(current.getNumberOfHighPriorityTasks(), current.getNumberOfNormalPriorityTasks(), current.getNumberOfLowPriorityTasks());
                 int level = 0;
                 for (Integer integer : series) {
-                    builder.add(integer, level, new NumberOnlyBuildLabel(action.owner));
+                    builder.add(integer, level, new NumberOnlyBuildLabel(action.getOwner()));
                     level++;
                 }
             }
