dfd3214f159e9652ba6699df45e912c6bd505a58
null <null@dev.java.net>
2007-10-15 18:33:11 +0000

    This commit was manufactured by cvs2svn to create tag 'tasks-1_13'.

---
+++ hudson/plugins/tasks/src/main/java/hudson/plugins/tasks/MavenJavaClassifier.java
@@ -0,0 +1,43 @@
+package hudson.plugins.tasks;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.LineIterator;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * Classifies a Java workspace file that is part of a maven module.
+ */
+public class MavenJavaClassifier implements FileClassifier {
+    /** {@inheritDoc} */
+    public void classify(final WorkspaceFile file, final InputStream stream) throws IOException {
+        try {
+            LineIterator iterator = IOUtils.lineIterator(stream, "UTF-8");
+            while (iterator.hasNext()) {
+                String line = iterator.nextLine();
+                if (line.matches("^package .*;$")) {
+                    file.setPackageName(StringUtils.substringBetween(line, " ", ";").trim());
+                    break;
+                }
+            }
+        }
+        finally {
+            IOUtils.closeQuietly(stream);
+        }
+        String module = StringUtils.substringBefore(file.getName(), "/src/");
+        if (module.contains("/")) {
+            module = StringUtils.substringAfterLast(module, "/");
+        }
+        if (StringUtils.isNotBlank(module)) {
+            file.setModuleName(module);
+        }
+    }
+
+    /** {@inheritDoc} */
+    public boolean accepts(final String fileName) {
+        return fileName.endsWith(".java");
+    }
+}
+
---
+++ hudson/plugins/tasks/src/main/java/hudson/plugins/tasks/TasksPlugin.java
@@ -0,0 +1,18 @@
+package hudson.plugins.tasks;
+
+import hudson.Plugin;
+import hudson.tasks.BuildStep;
+
+/**
+ * Registers the task scanner plug-in publisher.
+ *
+ * @author Ulli Hafner
+ */
+public class TasksPlugin extends Plugin {
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("PMD")
+    public void start() throws Exception {
+        BuildStep.PUBLISHERS.addRecorder(TasksPublisher.TASK_SCANNER_DESCRIPTOR);
+    }
+}
