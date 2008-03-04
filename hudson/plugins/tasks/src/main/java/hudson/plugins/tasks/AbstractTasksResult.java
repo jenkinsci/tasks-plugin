57e8acc75a3d6bc591f3ef7cfab6a1145f9d1570
null <null@dev.java.net>
2008-03-04 17:38:58 +0000

    This commit was manufactured by cvs2svn to create tag 'tasks-2_0'.

---
+++ hudson/plugins/tasks/src/main/java/hudson/plugins/tasks/AbstractTasksResult.java
@@ -0,0 +1,191 @@
+package hudson.plugins.tasks;
+
+import hudson.model.AbstractBuild;
+import hudson.model.ModelObject;
+import hudson.plugins.tasks.util.ChartBuilder;
+import hudson.plugins.tasks.util.model.AnnotationContainer;
+import hudson.plugins.tasks.util.model.AnnotationProvider;
+import hudson.plugins.tasks.util.model.FileAnnotation;
+import hudson.plugins.tasks.util.model.Priority;
+import hudson.util.ChartUtil;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.jfree.chart.JFreeChart;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+
+import edu.umd.cs.findbugs.annotations.SuppressWarnings;
+
+/**
+ * Base class for tasks detail objects.
+ */
+public abstract class AbstractTasksResult extends AnnotationContainer implements ModelObject {
+    /** The current build as owner of this action. */
+    @SuppressWarnings("Se")
+    private final AbstractBuild<?, ?> owner;
+
+    /** Tag identifiers indicating high priority. */
+    private final String high;
+    /** Tag identifiers indicating normal priority. */
+    private final String normal;
+    /** Tag identifiers indicating low priority. */
+    private final String low;
+
+    /**
+     * Creates a new instance of <code>AbstractTasksDetail</code>.
+     *
+     * @param owner
+     *            the current build as owner of this result object
+     * @param high
+     *            tag identifiers indicating high priority
+     * @param normal
+     *            tag identifiers indicating normal priority
+     * @param low
+     *            tag identifiers indicating low priority
+     * @param annotations
+     *            all the files that contain tasks
+     */
+    public AbstractTasksResult(final AbstractBuild<?, ?> owner, final Collection<FileAnnotation> annotations,
+            final String high, final String normal, final String low) {
+        super();
+
+        this.owner = owner;
+        this.high = high;
+        this.normal = normal;
+        this.low = low;
+
+        addAnnotations(annotations);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final void annotationAdded(final FileAnnotation annotation) {
+        // prevent overriding from sub classes
+    }
+
+    /**
+     * Returns the package category name for the scanned files. Currently, only
+     * java and c# files are supported.
+     *
+     * @return the package category name for the scanned files
+     */
+    public String getPackageCategoryName() {
+        if (hasAnnotations()) {
+            String fileName = getAnnotations().iterator().next().getFileName();
+            if (fileName.endsWith(".cs")) {
+                return "Namespace";
+            }
+        }
+        return "Package";
+    }
+
+    /**
+     * Returns the current build as owner of this result object.
+     *
+     * @return the owner of this details object
+     */
+    public final AbstractBuild<?, ?> getOwner() {
+        return owner;
+    }
+
+    /**
+     * Returns whether this result object belongs to the last build.
+     *
+     * @return <code>true</code> if this result belongs to the last build
+     */
+    public final boolean isCurrent() {
+        return getOwner().getProject().getLastBuild().number == getOwner().number;
+    }
+
+    /**
+     * Returns the actually used priorities.
+     *
+     * @return the actually used priorities.
+     */
+    public List<String> getPriorities() {
+        List<String> actualPriorities = new ArrayList<String>();
+        for (String priority : getAvailablePriorities()) {
+            if (getNumberOfAnnotations(priority) > 0) {
+                actualPriorities.add(priority);
+            }
+        }
+        return actualPriorities;
+    }
+
+    /**
+     * Returns the defined priorities.
+     *
+     * @return the defined priorities.
+     */
+    public Collection<String> getAvailablePriorities() {
+        ArrayList<String> priorities = new ArrayList<String>();
+        if (StringUtils.isNotEmpty(high)) {
+            priorities.add(StringUtils.capitalize(StringUtils.lowerCase(Priority.HIGH.name())));
+        }
+        if (StringUtils.isNotEmpty(normal)) {
+            priorities.add(StringUtils.capitalize(StringUtils.lowerCase(Priority.NORMAL.name())));
+        }
+        if (StringUtils.isNotEmpty(low)) {
+            priorities.add(StringUtils.capitalize(StringUtils.lowerCase(Priority.LOW.name())));
+        }
+        return priorities;
+    }
+
+    /**
+     * Returns the tags for the specified priority.
+     *
+     * @param priority the priority
+     *
+     * @return the tags for the specified priority
+     */
+    public final String getTags(final String priority) {
+        return getTags(Priority.fromString(priority));
+    }
+
+    /**
+     * Returns the tags for the specified priority.
+     *
+     * @param priority
+     *            the priority
+     * @return the tags for the specified priority
+     */
+    public final String getTags(final Priority priority) {
+        if (priority == Priority.HIGH) {
+            return high;
+        }
+        else if (priority == Priority.NORMAL) {
+            return normal;
+        }
+        else {
+            return low;
+        }
+    }
+
+    /**
+     * Creates a detail graph for the specified detail object.
+     *
+     * @param request Stapler request
+     * @param response Stapler response
+     * @param detailObject the detail object to compute the graph for
+     * @param upperBound the upper bound of all tasks
+     *
+     * @throws IOException in case of an error
+     */
+    protected final void createDetailGraph(final StaplerRequest request, final StaplerResponse response,
+            final AnnotationProvider detailObject, final int upperBound) throws IOException {
+        if (ChartUtil.awtProblem) {
+            response.sendRedirect2(request.getContextPath() + "/images/headless.png");
+            return;
+        }
+        JFreeChart chart = ChartBuilder.createHighNormalLowChart(
+                detailObject.getNumberOfAnnotations(Priority.HIGH),
+                detailObject.getNumberOfAnnotations(Priority.NORMAL),
+                detailObject.getNumberOfAnnotations(Priority.LOW), upperBound);
+        ChartUtil.generateGraph(request, response, chart, 400, 20);
+    }
+}
---
+++ hudson/plugins/tasks/src/main/java/hudson/plugins/tasks/TasksProjectAction.java
@@ -0,0 +1,42 @@
+package hudson.plugins.tasks;
+
+import hudson.model.AbstractProject;
+import hudson.plugins.tasks.util.AbstractProjectAction;
+
+/**
+ * Entry point to visualize the task scanner trend graph. Drawing of the graph is
+ * delegated to the associated {@link TasksResultAction}.
+ *
+ * @author Ulli Hafner
+ */
+public class TasksProjectAction extends AbstractProjectAction<TasksResultAction> {
+    /** Unique identifier of this class. */
+    private static final long serialVersionUID = 2726362508120270158L;
+
+    /**
+     * Instantiates a new tasks project action.
+     *
+     * @param project
+     *            the project that owns this action
+     */
+    public TasksProjectAction(final AbstractProject<?, ?> project) {
+        super(project, TasksResultAction.class, TasksDescriptor.TASKS_ACTION_LOGO, "../lastBuild/tasksResult");
+    }
+
+    /** {@inheritDoc} */
+    public String getDisplayName() {
+        return "Open Tasks";
+    }
+
+    /** {@inheritDoc} */
+    public String getUrlName() {
+        return "tasks";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getCookieName() {
+        return "Tasks_displayMode";
+    }
+}
+
