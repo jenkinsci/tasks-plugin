624530a8d4d813d22d0af8ebd5039408c26b1459
drulli <drulli@dev.java.net>
2008-05-13 17:40:07 +0000

    Fixed typo in class name that prevents reading of the config file.

--- src/main/java/hudson/plugins/tasks/ResultSummary.java
+++ src/main/java/hudson/plugins/tasks/ResultSummary.java
@@ -2,7 +2,7 @@ package hudson.plugins.tasks;
 
 
 /**
- * Represents the result summary of the FindBugs parser. This summary will be
+ * Represents the result summary of the Tasks parser. This summary will be
  * shown in the summary.jelly script of the FindBugs result action.
  *
  * @author Ulli Hafner
