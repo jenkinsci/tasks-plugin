6cbe2e0f0d7b330a9e99166762a40f76da045c38
drulli <drulli@dev.java.net>
2008-12-28 17:47:48 +0000

    Aggregate error messages in parent modules.

--- src/main/java/hudson/plugins/tasks/TasksResult.java
+++ src/main/java/hudson/plugins/tasks/TasksResult.java
@@ -108,7 +108,7 @@ public class TasksResult extends BuildResult  {
      *            tag identifiers indicating low priority
      */
     public TasksResult(final AbstractBuild<?, ?> build, final String defaultEncoding, final TasksParserResult project, final int previousNumberOfTasks, final String high, final String normal, final String low) {
-        super(build, project.getModules(), defaultEncoding);
+        super(build, project.getModules(), project.getErrorMessages(), defaultEncoding);
 
         highPriorityTasks = project.getNumberOfAnnotations(Priority.HIGH);
         lowPriorityTasks = project.getNumberOfAnnotations(Priority.LOW);
