cfdbf796f3f3cca9a0bb21c2f81e8fc2d71eb187
drulli <drulli@dev.java.net>
2010-05-25 14:02:33 +0000

    Removed deprecated API calls.

--- src/main/java/hudson/plugins/tasks/TasksReporter.java
+++ src/main/java/hudson/plugins/tasks/TasksReporter.java
@@ -15,6 +15,7 @@ import hudson.plugins.tasks.parser.WorkspaceScanner;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
@@ -223,8 +224,8 @@ public class TasksReporter extends HealthAwareMavenReporter {
 
     /** {@inheritDoc} */
     @Override
-    public Action getProjectAction(final MavenModule module) {
-        return new TasksProjectAction(module);
+    public List<TasksProjectAction> getProjectActions(final MavenModule module) {
+        return Collections.singletonList(new TasksProjectAction(module));
     }
 
     /** {@inheritDoc} */
