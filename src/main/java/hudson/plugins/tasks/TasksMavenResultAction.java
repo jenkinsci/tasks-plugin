75637499df9343bfaf6ddc7a86ed9563788fdb51
Ulli Hafner <ullrich.hafner@gmail.com>
2012-09-24 23:33:16 +0200

    Fixed NPE.

--- src/main/java/hudson/plugins/tasks/TasksMavenResultAction.java
+++ src/main/java/hudson/plugins/tasks/TasksMavenResultAction.java
@@ -6,6 +6,7 @@ import hudson.maven.MavenModule;
 import hudson.maven.MavenModuleSet;
 import hudson.maven.MavenModuleSetBuild;
 import hudson.model.Action;
+import hudson.model.AbstractBuild;
 import hudson.plugins.analysis.core.HealthDescriptor;
 import hudson.plugins.analysis.core.MavenResultAction;
 import hudson.plugins.tasks.parser.TasksParserResult;
@@ -31,31 +32,6 @@ public class TasksMavenResultAction extends MavenResultAction<TasksResult> {
     private String low;
 
     /**
-     * Creates a new instance of {@link TasksMavenResultAction}. This instance
-     * will have no result set in the beginning. The result will be set
-     * successively after each of the modules are build.
-     *
-     * @param owner
-     *            the associated build of this action
-     * @param healthDescriptor
-     *            health descriptor to use
-     * @param defaultEncoding
-     *            the default encoding to be used when reading and parsing files
-     * @param high
-     *            tag identifiers indicating high priority
-     * @param normal
-     *            tag identifiers indicating normal priority
-     * @param low
-     *            tag identifiers indicating low priority
-     */
-    public TasksMavenResultAction(final MavenModuleSetBuild owner, final HealthDescriptor healthDescriptor,
-            final String defaultEncoding, final String high, final String normal, final String low) {
-        super(new TasksResultAction(owner, healthDescriptor), defaultEncoding, "TASKS");
-
-        initializeFields(high, normal, low);
-    }
-
-    /**
      * Creates a new instance of {@link TasksMavenResultAction}.
      *
      * @param owner
@@ -73,7 +49,7 @@ public class TasksMavenResultAction extends MavenResultAction<TasksResult> {
      * @param result
      *            the result in this build
      */
-    public TasksMavenResultAction(final MavenBuild owner, final HealthDescriptor healthDescriptor,
+    public TasksMavenResultAction(final AbstractBuild<?, ?> owner, final HealthDescriptor healthDescriptor,
             final String defaultEncoding, final String high, final String normal, final String low, final TasksResult result) {
         super(new TasksResultAction(owner, healthDescriptor, result), defaultEncoding, "TASKS");
 
@@ -100,7 +76,8 @@ public class TasksMavenResultAction extends MavenResultAction<TasksResult> {
 
     /** {@inheritDoc} */
     public MavenAggregatedReport createAggregatedAction(final MavenModuleSetBuild build, final Map<MavenModule, List<MavenBuild>> moduleBuilds) {
-        return new TasksMavenResultAction(build, getHealthDescriptor(), getDefaultEncoding(), high, normal, low);
+        return new TasksMavenResultAction(build, getHealthDescriptor(), getDefaultEncoding(), high, normal, low,
+                new TasksResult(build, high, new TasksParserResult(), high, normal, low));
     }
 
     /** {@inheritDoc} */
